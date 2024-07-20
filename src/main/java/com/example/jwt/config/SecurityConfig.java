47732c84086018a0008da968955341406446fdff
postwo <codo7717@naver.com>
2024-07-20 17:28:21 +0900

    jwt서버 security설정방법

---
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfig corsConfig;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                        .requestMatchers("/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .disable()
+                )
+                .httpBasic(basic -> basic
+                        .disable()
+                )
+                .addFilter(corsConfig.corsFilter());//@CrossOrigin(인증x),시큐리티 필터에 등록 인증(O) = 모든요청을 허용
+
+
+        return http.build();
+    }
+}
