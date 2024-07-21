105ca2c51b8a91c62c307dd018db1b0afd7151f2
postwo <codo7717@naver.com>
2024-07-21 15:35:37 +0900

    이방식으로 jwt 사용 하지말기

--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.jwt.config;
 
+import com.example.jwt.config.jwt.JwtAuthenticationFilter;
+import com.example.jwt.config.jwt.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -17,11 +21,16 @@ public class SecurityConfig {
     private final CorsConfig corsConfig;
 
     @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //session사용 x
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
@@ -29,13 +38,13 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
-                .formLogin(form -> form
-                        .disable()
+                //security자체로그인 페이지 사용 x
+                .formLogin(AbstractHttpConfigurer::disable
                 )
-                .httpBasic(basic -> basic
-                        .disable()
+                //
+                .httpBasic(AbstractHttpConfigurer::disable
                 )
-                .addFilter(corsConfig.corsFilter());//@CrossOrigin(인증x),시큐리티 필터에 등록 인증(O) = 모든요청을 허용
+                .addFilter(corsConfig.corsFilter())//@CrossOrigin(인증x),시큐리티 필터에 등록 인증(O) = 모든요청을 허용
 
 
         return http.build();
